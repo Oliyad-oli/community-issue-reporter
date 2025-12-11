ad684f3e9c598367d4c0c1c5b9e6df39f921b88f
Oliyad-oli <oliidan657@gmail,com>
2025-12-12 00:12:31 +0300

    Fix routing using HashRouter

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,13 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+      base: "/community-issue-reporter/",
+
   server: {
     host: "::",
     port: 8080,
+    base: "/community-issue-reporter/",
+  plugins: [react()],
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
